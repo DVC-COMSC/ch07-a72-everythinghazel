aaccf384d553657b9b563db55c09bd4944d44e4b
Hazel Lee <hyewon1157@gmail.com>
2022-11-27 13:00:41 -0800

    chpt7-2

--- main.py
+++ main.py
@@ -3,8 +3,11 @@ num1 = list(map(int, input().split()))
 num2 = list(map(int, input().split()))
 
 # ******************************
+flag = 0
 for i in range(len(num2)):
     if num2[i] not in num1:
+        continue
+    else:
         print('False')
         break
 else:
