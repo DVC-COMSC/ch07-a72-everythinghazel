c4a588653690935807488edcbef45bac95027204
Hazel Lee <hyewon1157@gmail.com>
2022-11-29 13:37:31 -0800

    7-2

--- main.py
+++ main.py
@@ -6,12 +6,15 @@ num2 = list(map(int, input().split()))
 flag = 0
 for i in range(len(num2)):
     if num2[i] not in num1:
-        continue
-    else:
-        print('False')
+        print(False)
         break
-else:
-    print('true')
-# ******************************
+    else:
+        if i == len(num2)-1:
+            print(True)
+        else: 
+            continue
+
+        
+ 
 
 # print ('True') or print ('False')
